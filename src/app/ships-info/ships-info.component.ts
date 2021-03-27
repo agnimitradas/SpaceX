f6872599b42d63fbcd231dd88733c33bf959d7f6
Agnimitra Das <agnimitradas@outlook.com>
2021-03-27 21:22:59 +0530

    Styles added

--- src/app/ships-info/ships-info.component.ts
+++ src/app/ships-info/ships-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { take } from "rxjs/operators";
+import { Subject } from "rxjs";
+import { switchMap, takeUntil } from "rxjs/operators";
 import { ShipsService } from "./ships-info.service";
 
 @Component({
@@ -10,20 +11,35 @@ import { ShipsService } from "./ships-info.service";
 })
 export class ShipsComponent implements OnInit {
   shipsList: Array<any> = [];
+  loading: boolean = true;
+  shipsDetailsChange: Subject<any> = new Subject<any>();
+  private unsub: Subject<any> = new Subject<any>();
 
   constructor(private shipsSvc: ShipsService) {}
 
   ngOnInit(): void {
     this.getShipsList();
+    this.shipsDetailsChange.next({});
   }
 
   getShipsList(params?: any) {
-    this.shipsSvc
-      .getShipsList(params)
-      .pipe(take(1))
-      .subscribe((data: Array<any>) => {
-        this.shipsList = data;
-      });
+    this.loading = true;
+    /* Switch map will cancel the pending requests. I.E. if user keep on clicking filters, We should only consider the latest one. 
+    Everything else has to be cancelled */
+    this.shipsDetailsChange
+      .pipe(
+        takeUntil(this.unsub),
+        switchMap((params) => this.shipsSvc.getShipsList(params))
+      )
+      .subscribe(
+        (data: Array<any>) => {
+          this.shipsList = data;
+          this.loading = false;
+        },
+        (error) => {
+          this.loading = false;
+        }
+      );
   }
 
   filterChange(filter: {
@@ -36,6 +52,6 @@ export class ShipsComponent implements OnInit {
     if (filter.launchSuccess != null) params.launch_success = filter.year;
     if (filter.launchSuccess != null) params.land_success = filter.year;
 
-    this.getShipsList(params);
+    this.shipsDetailsChange.next(params);
   }
 }
